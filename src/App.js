0c829ad0071d6059bfcf70ec6ee0eb3d0a03f9c7
Quang Bui <77463586+duyquangbui99@users.noreply.github.com>
2023-11-06 14:43:11 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import './index.css';
+
+
+
+export default function App() {
+  const [newName, setNewItem] = useState("")
+  const [listName, setlistName] = useState([])
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (newName) {
+      setlistName([...listName, newName]); // add new name to the list array
+      setNewItem(""); //clear the input field
+    }
+
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className='form-row'>
+          <label htmlFor='item'>Name</label>
+          <input value={newName} onChange={e => setNewItem(e.target.value)} type="text" id='item' />
+          <button className='btn'>Add</button>
+        </div>
+      </form>
+      <h1 className='header'>List</h1>
+      <ul className='list'>
+        {listName.map((name, index) => (
+          <li key={index}>
+            <div>{name}</div>
+            <button className='btn btn-danger'>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
